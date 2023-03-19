2a959df46b6077459a59bda89dc94e85a05a9680
qasklp <freeee386@gmail.com>
2023-03-19 04:15:30 +0200

    remove local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+
 
 import { Phonebook } from "./Phonebook/Phonebook";
-import { store, persistor } from "Redux/store";
+import { store } from "Redux/store";
 
 
 export const App = () => {
   return <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <Phonebook />
-    </PersistGate>
+    <Phonebook />
   </Provider>;
 }
